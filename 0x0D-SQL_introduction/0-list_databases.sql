86b93b1a6e52709172743335741c4ec9814df49c
YisehakKEK <yisehakschool@gmail.com>
2024-11-20 09:03:18 +0300

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,7 @@
+-- 0. List databases
+-- This script lists all databases of the MySQL server.
+SHOW DATABASES;
+
+-- Testing the script
+-- To test, run the following command in your terminal:
+-- $ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
