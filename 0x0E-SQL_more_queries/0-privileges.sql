083d154c03417e7c88330301df79382e0f4da18f
Yisehak Kassahun <yisehakschool@gmail.com>
2024-11-20 06:36:52 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
--- Script to list all privileges for users user_0d_1 and user_0d_2
+-- Task: List all privileges of MySQL users `user_0d_1` and `user_0d_2` on localhost
+-- This script displays the privileges for the specified users
+
+-- Display privileges for `user_0d_1`
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Display privileges for `user_0d_2`
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
