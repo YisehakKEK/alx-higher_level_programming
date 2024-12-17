38aeabc8e10d94a9bf3fb47fc8ec7db1687d385d
YisehakKEK <yisehakschool@gmail.com>
2024-12-17 16:47:35 +0300

    Revision

--- 0-list_databases.sql
+++
@@ -1,7 +0,0 @@
--- 0. List databases
--- This script lists all databases of the MySQL server.
-SHOW DATABASES;
-
--- Testing the script
--- To test, run the following command in your terminal:
--- $ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
--- README.md
+++
@@ -1,3 +0,0 @@
-# MySQL Project
-This repository contains SQL scripts for managing and interacting with databases using MySQL 8.0 on Ubuntu 20.04 LTS.
-
